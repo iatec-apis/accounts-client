67033cfb18b30ed58ed045dd147d1d7219693d34
Andre Guilherme <andre.guilherme@iatec.com>
2017-06-21 10:50:12 -0300

    Remove nuget binary

--- generate.bat
+++ generate.bat
@@ -1,4 +1,3 @@
 @echo off
 rmdir /s/q docs src\Iatec.Apis.Accounts.v1.Client .vs
-del build.* *.yml *.sh
 java -jar "generator\swagger-codegen-cli.jar" generate -l csharp -i "https://ws-accounts-dev.sdasystems.org/docs/static/spec.yaml" --config "generator\config.json" --model-name-suffix=Model
